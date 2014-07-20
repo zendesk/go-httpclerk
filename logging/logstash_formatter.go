15bade0b6ac6fddbe672b81ef6c10cae41c539be
Ciarán Archer <carcher@zendesk.com>
2014-07-20 12:27:38 +0100

    Implement a configurable request logger.
    This PR creates a RequestLogger type which logs important request
    information.
    
    This type can be passed a specific logger (Syslog, StdOut etc.) to
    which it writes.
    
    The type is also passed a Formatter interface type, of which I have
    implemented the LogStashFormatter attempting to mirror the current
    Voice LogStash implementation. You can see a comparison here:
    
    https://gist.github.com/ciaranarcher/f241539c5373e3e5e001
    
    When testing I use a logger that wraps an in-memory buffer to
    allow easy testing. For realz in the app I've included a simple
    level-logger implementation you can find here:
    
    http://godoc.org/github.com/tideland/goas/v2/logger

---
+++ logging/logstash_formatter.go
@@ -0,0 +1,41 @@
+package logging
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"time"
+)
+
+// Logstash formatter
+type LogStashFormatter struct {
+	Source string
+	Tags   []string
+}
+
+func NewLogStashFormatter(source string, tags []string) (*LogStashFormatter, error) {
+	return &LogStashFormatter{Source: source, Tags: tags}, nil
+}
+
+type LogStashJSON struct {
+	Source    string      `json:"@source"`
+	Fields    interface{} `json:"@fields"`
+	Tags      []string    `json:"@tags"`
+	Timestamp string      `json:"@timestamp"`
+}
+
+func (formatter *LogStashFormatter) Format(customFields interface{}) (string, error) {
+	stash := &LogStashJSON{
+		Source:    formatter.Source,
+		Fields:    customFields,
+		Tags:      formatter.Tags,
+		Timestamp: time.Now().Format(time.RFC3339Nano),
+	}
+
+	data, err := json.Marshal(stash)
+	if err != nil {
+		return "", errors.New(fmt.Sprintf("Error marshalling JSON: %s", err))
+	}
+
+	return string(data), nil
+}
